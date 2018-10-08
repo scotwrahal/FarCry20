7706c6d61b5c55eae6f5f7f661b3a3cafd534c15
Scot Rahal <scot.rahal@ucalgary.ca>
2018-10-08 13:53:36 -0600

    Added the first test program that prints a character

---
+++ ek_test0.asm
@@ -0,0 +1,19 @@
+;ek_test0.asm
+	processor	6502				;assembling for 6502
+	org			$1001				;standard organization	
+	dc.w		end_basic			;pointer to end of BASIC stub
+	dc.w		1234				;line number 1234
+	
+	dc.b		$9e					;SYS instruction
+	dc.b 		$34,$31,$30,$39		;address given to SYS: 4109 in ascii
+	dc.b 		0                   ;null terminator for SYS statement  
+
+end_basic
+	dc.w		0					;indicating end of BASIC stub
+	
+start
+	lda #66							;load ascii 'B' into reg A
+	jsr $ffd2						;jump to print subroutine
+	jmp			start				;repeat
+	rts
+	
