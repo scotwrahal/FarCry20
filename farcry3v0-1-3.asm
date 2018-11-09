b353c07c8eabcfc006eea11786dd6a67c132470d
Scot Rahal <scot.rahal@ucalgary.ca>
2018-11-09 13:44:59 -0700

    updated the move function and reorganized some functions

--- farcry3v0-1-3.asm
+++ farcry3v0-1-3.asm
@@ -72,7 +72,6 @@ B3next
 	jmp load_row
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 set_timers
-
 play_loop
 	jsr input		
 	jsr movePlayer
@@ -95,7 +94,7 @@ loadByte
 	tax
 	pla
 	rts
-
+	
 level_load_done
 	pla
 	tax
@@ -107,12 +106,11 @@ movePlayer
 	tya
 	pha
 	ldy #1
-	lda updatable_entity_mem
+	lda updatable_entity_mem		; Player is the First updatable entity
 	sta $fe
-	lda updatable_entity_mem,y
+	lda updatable_entity_mem,y	;
 	sta $ff
-	ldy #5
-	lda player_direction,y 	;get the player direction
+	lda player_direction 	;get the player direction
 	jsr move
 	pla
 	tay
@@ -122,16 +120,26 @@ movePlayer
 	
 ;move
 ;	A: direction
-;	fe ff: memorylocation of entity to be moved 
+;	fe ff: memorylocation of entity to be moved
 move
 	sta holder
 	pha
 	txa
 	pha
-	tya
+	tya	
 	pha
-	lda holder
-	ldy #4			; this is the offest for the position
+	ldy position_offset			; this is the offest for the position in the structure
+	
+	; transfer the old position to the new position
+	lda ($fe),y					; load the location
+	sta new_position
+	iny 
+	ldx #1
+	lda ($fe),y
+	sta new_position,x
+	ldy #1
+	
+	lda holder					; restore the direction into A
 	asl
 	bcs MoveUp
 	asl
@@ -140,117 +148,140 @@ move
 	bcs	MoveLeft
 	asl
 	bcs MoveRight
-	jmp EndMove
-	rts
+	jmp EndMove				; if there is no direction then it doesn't move
 	
 ; MoveUp is commented the others moves follow similar logic
 MoveUp
-	lda ($fe),y				; load the location
+	lda new_position,y
 	sec
 	sbc #22					; move by one row
 	bcc	MoveUpBorder		; check if you cross the upper lower border
-	ldy #1
 	sta new_position,y		; if you didn't, store the new locaiton
 	jmp FinishMove
 MoveUpBorder
-	ldy #3
-	lda ($fe),y				; check if you are in the top or bottom
+	lda new_position
 	beq	FinishMove			; if you are in the top then you cant move up
 	lda #0					; you are now in the top
 	sta new_position		; save in new positon
-	iny
-	lda ($fe),y
+	lda new_position,y
 	sec
 	sbc #22					; move up one row 
-	sta new_position,y		; save in new position
+	sta new_position,y		; save new position
 	jmp FinishMove
 	
 MoveDown
-	lda ($fe),y
+	lda new_position,y
 	clc
 	adc #22
 	bcs	MoveDownBorder
-	ldy	#1
 	sta new_position,y
 	jmp FinishMove
 MoveDownBorder
-	ldy #3
-	lda ($fe),y
+	lda new_position
 	bne	FinishMove
 	lda #1
 	sta new_position
-	iny
-	lda ($fe),y
+	lda new_position,y
 	clc
 	adc #22
 	sta new_position,y
 	jmp FinishMove
 	
 MoveLeft
-	lda ($fe),y
+	lda new_position,y
 	sec
 	sbc #1
 	bcc MoveLeftBorder
-	ldy #1
 	sta new_position,y
 	jmp FinishMove
 MoveLeftBorder
-	ldy #3
-	lda ($fe),y
+	lda new_position
 	beq	FinishMove
 	lda #0
 	sta new_position
 	lda #$ff
-	iny
 	sta new_position,y
 	jmp FinishMove
 	
 MoveRight
-	lda ($fe),y
+	lda new_position,y
 	clc
 	adc #1
 	bcs MoveRightBorder
-	ldy #1
 	sta new_position,y
 	jmp FinishMove
 MoveRightBorder
-	ldy #3
-	lda player_position
+	lda new_position
 	bne	FinishMove
 	lda #1
 	sta new_position
 	lda	#0
-	iny
 	sta new_position,y
 	jmp FinishMove
 	
 FinishMove
 	jsr check_collision
+	cmp #0
 	bne Collision
 	
-	ldy #1
+	ldy position_offset
+	iny
 	lda ($fe),y
 	tax
-	ldy #0
+	dey
 	lda ($fe),y
 	jsr	drawGround
 	
-	
-	ldy #0
+	ldy #1
+	lda new_position,y
+	ldy position_offset
+	iny
+	sta ($fe),y
+	tax
+	dey
 	lda new_position
 	sta ($fe),y
-	iny
-	lda new_position,y
-	sta ($fe),y
 	jsr draw
+
 Collision
 
-EndMove	
+EndMove
 	pla
 	tay
 	pla
 	tax
 	pla
+	rts
+
+
+check_collision
+	pha
+	tya
+	pha
+	lda new_position
+	ldy #1
+	ldx new_position,y
+	jsr getFromPosition
+	; need to make a collide list maybe
+	cmp ground_char
+	beq NoCollide
+	ldy position_offset
+	lda ($fe),y
+	sta new_position
+	iny
+	lda ($fe),y
+	ldy #1
+	sta new_position,y
+	pla
+	tay
+	pla
+	lda #1
+	rts
+NoCollide
+	pla
+	tay
+	pla
+	lda #0
 	rts
 	
 ;getFromPosition
@@ -276,61 +307,22 @@ readBottom
 	pla
 	rts
 	
-	
-drawGround
-	sta holder
-	pha
-	tya
-	pha
-	ldy #0
-	lda drawable_mem,y
-	sta $fe
-	iny
-	lda	drawable_mem,y
-	sta $ff
-	lda holder
-	jsr draw
-	pla
-	tay
-	pla
-	rts
-
-
-drawTerrain
-	sta holder
-	pha
-	tya
-	pha
-	ldy #2
-	lda drawable_mem,y
-	sta $fe
-	iny
-	lda	drawable_mem,y
-	sta $ff
-	lda holder
-	jsr draw
-	pla
-	tay
-	pla
-	rts
-	
-	
 ;draw
 	; A: top/bottom location 0 = top
 	; X: location
 	; $fe $ff: entity to draw 
 draw
-	sta holder
+	sta holder			; clear accumulator to store state
 	pha
 	tya
 	pha
 	lda holder
 	cmp #0
 	bne DrawBottom
-	ldy #0
+	ldy #0				; this is the index for the character
 	lda ($fe),y
 	sta	$1e00,x
-	ldy #1
+	ldy #1				; this is the index for the color
 	lda ($fe),y
 	sta $9600,x
 	jmp	EndDraw
@@ -348,6 +340,69 @@ EndDraw
 	pla
 	rts
 
+; drawTerrain
+; A: top/bottom bit 0= top
+; X: position
+drawTerrain
+	sta holder
+	pha
+	tya
+	pha
+	ldy #0
+	lda holder
+	jsr drawDrawable
+	pla
+	tay
+	pla
+	rts
+	
+; drawGround
+; A: top/bottom bit 0= top
+; X: position	
+drawGround
+	sta holder
+	pha
+	tya
+	pha
+	ldy #1
+	lda holder
+	jsr drawDrawable
+	pla
+	tay
+	pla
+	rts
+
+; drawTerrain
+; A: top/bottom bit 0= top
+; X: position
+; Y: index
+drawDrawable
+	sta holder
+	pha
+	lda	$ff
+	pha
+	lda	$fe
+	pha
+	tya
+	pha
+	asl
+	tay					;multiply by 2
+	lda drawable_mem,y
+	sta $fe
+	iny
+	lda	drawable_mem,y
+	sta $ff
+	lda holder
+	jsr draw
+	pla
+	tay
+	pla
+	sta	$fe
+	pla
+	sta	$ff
+	pla
+	rts
+	
 ; input
 ; stores player in last_input
 input
@@ -380,28 +435,6 @@ InputRetrun
 	sta player_direction
 	pla
 	rts
-
-check_collision
-	lda new_position
-	ldx new_position,y
-	jsr getFromPosition
-
-	; need to make a collide list maybe
-	cmp ground_char
-	beq NoCollide
-	
-	ldy #0
-	lda ($fe),y
-	sta new_position
-	ldy #1
-	lda ($fe),y
-	sta new_position,y
-	lda #1
-	rts
-	
-NoCollide
-	lda #0
-	rts
 	
 holder
 	dc.b	$00
@@ -417,12 +450,15 @@ updatable_entity_mem
 	dc.w 	player_char
 	dc.w	$0000
 	
+position_offset
+	dc.b	$02
+	
 player_char      
 	dc.b	$51
 player_color     
 	dc.b	$06
 player_position
-	dc.b	$01, $00
+	dc.b	$00, $22
 player_direction
 	dc.b	$00
 player_clock_updates
@@ -434,6 +470,7 @@ player_clock
 drawable_mem
 	dc.w	terrain_char
 	dc.w	ground_char
+	dc.w 	player_char
 	dc.w	$0000
 	
 terrain_char
